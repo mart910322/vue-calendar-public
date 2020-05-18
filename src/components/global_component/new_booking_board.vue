d056daf41a851c748d0919392f68489dd97f1e09
meisken <meisken999@gmail.com>
2020-05-18 08:04:27 +0800

    merge is finished 2

--- src/components/global_component/new_booking_board.vue
+++ src/components/global_component/new_booking_board.vue
@@ -359,11 +359,7 @@ export default {
                     this.endingAction(true);
         
                     this.errorText = err;
-<<<<<<< HEAD
-                    console.log(err );
-=======
                     console.log(err  );
->>>>>>> master
                 })
    
             }
